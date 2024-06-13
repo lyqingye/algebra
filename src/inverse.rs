a89e9e1de7ff574aaf818db2de2ac72f37abe43a
lyqingye <lyqingye@users.noreply.github.com>
2024-06-13 09:42:16 +0000

    fix

--- src/inverse.rs
+++ src/inverse.rs
@@ -1,7 +1,7 @@
 use crate::ext_euc::ext_euc;
 
-fn inverse(a: i64, m: u64) -> Option<u64> {
-    let (d, x, y) = ext_euc(a, m as i64);
+fn modular_inverse(a: i64, m: u64) -> Option<u64> {
+    let (d, x, _y) = ext_euc(a, m as i64);
     if d == 1 {
         if x < 0 {
             Some((x + m as i64) as u64)
@@ -18,14 +18,14 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_inverse() {
-        assert_eq!(Some(5), inverse(3, 7));
-        assert_eq!(Some(7), inverse(3, 10));
-        assert_eq!(Some(15), inverse(7, 26));
+    fn test_modular_inverse() {
+        assert_eq!(Some(5), modular_inverse(3, 7));
+        assert_eq!(Some(7), modular_inverse(3, 10));
+        assert_eq!(Some(15), modular_inverse(7, 26));
 
-        assert_eq!(None, inverse(2, 6));
-        assert_eq!(None, inverse(6, 12));
-        assert_eq!(None, inverse(5, 15));
-        assert_eq!(None, inverse(7, 21));
+        assert_eq!(None, modular_inverse(2, 6));
+        assert_eq!(None, modular_inverse(6, 12));
+        assert_eq!(None, modular_inverse(5, 15));
+        assert_eq!(None, modular_inverse(7, 21));
     }
 }
