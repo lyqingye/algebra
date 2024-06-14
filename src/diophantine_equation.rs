d38736f3424b2d5d3f44c09032f31188cb14af5a
lyqingye <lyqingye@users.noreply.github.com>
2024-06-14 03:10:54 +0000

    feat: solve diophantine equiation

---
+++ src/diophantine_equation.rs
@@ -0,0 +1,23 @@
+use crate::ext_euc::ext_euc;
+
+fn solve_diophantine_equation(a: i64, b: i64, c: i64) -> Option<(i64, i64)> {
+    let(d,x_0,y_0) = ext_euc(a,b);
+
+    if c % d == 0 {
+        let k = c / d;
+        Some((x_0 * k, y_0 * k))
+    }else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_solve_diophantine_equation() {
+        assert_eq!(Some((-5,5)),solve_diophantine_equation(3,4,5));
+        assert_eq!(Some((168498,-2048886)),solve_diophantine_equation(51241,4214,414));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod exponent;
 mod ext_euc;
 mod gcd;
 mod inverse;
+mod diophantine_equation;
 
 fn main() {
     println!("Hello, world!");
