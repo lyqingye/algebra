0cc61240574397322d8ce2299095382d0e93bf95
lyqingye <lyqingye@users.noreply.github.com>
2024-07-01 15:59:46 +0000

    feat: binary gcd for bignum

--- src/num/gcd.rs
+++ src/num/gcd.rs
@@ -1,3 +1,5 @@
+use std::cmp::{min, Ordering};
+
 use crate::num::uint::Uint;
 
 #[inline(always)]
@@ -16,9 +18,47 @@ pub fn gcd<const LIMBS: usize>(a: &Uint<LIMBS>, b: &Uint<LIMBS>) -> Uint<LIMBS> 
     }
 }
 
+#[inline(always)]
+pub fn binary_gcd<const LIMBS: usize>(a: &Uint<LIMBS>, b: &Uint<LIMBS>) -> Uint<LIMBS> {
+    if a.is_zero() {
+        return *b;
+    }
+
+    if b.is_zero() {
+        return *a;
+    }
+
+    let mut u = *a;
+    let mut v = *b;
+
+    let i = u.trailing_zeros();
+    let j = v.trailing_zeros();
+    let k = min(i, j);
+    // let k = u.bitor(&v).trailing_zeros();
+
+    u = u.wrapping_shr(i);
+    v = v.wrapping_shr(j);
+
+    loop {
+        match u.cmp(&v) {
+            Ordering::Greater => {
+                u = u - &v;
+                u = u.wrapping_shr(u.trailing_zeros());
+            }
+            Ordering::Less => {
+                v = v - &u;
+                v = v.wrapping_shr(v.trailing_zeros());
+            }
+            Ordering::Equal => break,
+        };
+    }
+
+    u.wrapping_shl(k)
+}
+
 #[cfg(test)]
 mod test {
-    use crate::num::gcd::gcd;
+    use crate::num::gcd::{binary_gcd, gcd};
     use crate::num::uint::U128;
 
     #[test]
@@ -28,5 +68,17 @@ mod test {
             gcd(&U128::from(148u64), &U128::from(36u64))
         );
         assert_eq!(U128::from(5u64), gcd(&U128::from(15u64), &U128::from(5u64)));
+    }
+
+    #[test]
+    fn test_binary_gcd() {
+        assert_eq!(
+            U128::from(4u64),
+            binary_gcd(&U128::from(148u64), &U128::from(36u64))
+        );
+        assert_eq!(
+            U128::from(5u64),
+            binary_gcd(&U128::from(15u64), &U128::from(5u64))
+        );
     }
 }
